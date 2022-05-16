87473dd5e0924376ac4d9e7b92986205af5eef0d
Ewa Kadziolka <kalistaewa@gmail.com>
2022-05-17 00:12:31 +0200

    firsts working routes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import MongoDBteddy from "./config/db.js";
+import authRoute from "./routes/auth.js";
+import itemsRoute from "./routes/items.js";
+import usersRoute from "./routes/users.js";
+import messagesRoute from "./routes/messages.js";
 
 const port = parseInt(process.env.PORT, 10) || 5000;
 const app = express()
@@ -9,6 +13,12 @@ dotenv.config()
 app.get('/', (req, res, next)=> {
     res.json({status: 200, msg: "ok"})
 })
+
+//middlewares
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/items", itemsRoute);
+app.use("/api/v1/users", usersRoute);
+app.use("/api/v1/messages", messagesRoute);
 
 
 app.listen(port, ()=>{
