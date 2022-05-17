edc91e99b980c65f1a2777ae0ca691e14ab1e61c
Ewa Kadziolka <kalistaewa@gmail.com>
2022-05-17 11:38:48 +0200

    errors gestion

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ app.use("/api/v1/items", itemsRoute);
 app.use("/api/v1/users", usersRoute);
 app.use("/api/v1/messages", messagesRoute);
 
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack,
+    });
+  });
+
 
 app.listen(port, ()=>{
     console.log(`it works on port ${port}`)
