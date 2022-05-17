d498fc4e2a5d6f3ad0ccd47c5f15c610cf4e999a
Ewa Kadziolka <kalistaewa@gmail.com>
2022-05-17 13:52:39 +0200

    add controllers to items

--- routes/items.js
+++ routes/items.js
@@ -1,64 +1,20 @@
 import express  from "express";
 import Item from "../models/Item.js";
+import { 
+  createItem,
+  deleteItem,
+  getAllItems,
+  getItemById,
+  updateItem,
+ } from "../controllers/itemControllers.js";
 
 const router = express.Router();
 
-//CREATE
-router.post("/", async(req, res)=>{
-
-    const newItem = new Item(req.body);
-  
-    try {
-      const savedItem = await newItem.save();
-      res.status(200).json(savedItem);
-    } catch (err) {
-     res.status(500).json(err)
-    }
-
-})
-//UPDATE
-router.put("/:id", async(req, res)=>{
-  try {
-    const updateItem = await Item.findByIdAndUpdate(
-      req.params.id, 
-      {$set: req.body},
-      {new: true})
-    res.status(200).json(updateItem)
-    
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
-//DELETE
-router.delete("/:id", async(req, res)=>{
-  try {
-    await Item.findByIdAndDelete(req.params.id);
-    res.status(200).json("Item has been delated")
-    
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
-//GET
-router.get("/:id", async(req, res)=>{
-  try {
-    const item = await Item.findById(req.params.id);
-    res.status(200).json(item)
-    
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
-//GET ALL
-router.get("/", async(req, res)=>{
-  try {
-    const items = await Item.find(req.params.id);
-    res.status(200).json(items)
-    
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
+router.post("/", createItem);
+router.put("/:id", updateItem);
+router.delete("/:id", deleteItem);
+router.get("/:id", getItemById)
+router.get("/", getAllItems)
 
 
 export default router
