ce2139dfb8e2787d7afbebca2cb50ff8cf7a1348
Ewa Kadziolka <kalistaewa@gmail.com>
2022-05-21 00:19:03 +0200

    distructures user

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -29,7 +29,8 @@ export const login = async(req,res,next) =>{
     const isPassowrdCorrect = await bcrypt.compare(req.body.password, user.password);
     if (!isPassowrdCorrect) return next(createError(400, "Wrong password or username!"));
 
-    res.status(200).json(user);
+      const {password, isAdmin, ...otherDetails} = user._doc
+    res.status(200).json({...otherDetails});
   } catch (err) {
     next(err);
   }
